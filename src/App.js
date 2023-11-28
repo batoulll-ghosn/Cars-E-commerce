342ec00712ce3d03bfc85afa84eacfe20d3a74e1
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-11-28 15:46:14 +0200

    about done with responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './components/mainComponents/AboutSection';
+import AboutPage from './components/mainComponents/AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/mainComponents/AboutSection.js
@@ -0,0 +1,13 @@
+import '../styles/about.css'
+function About() {
+  return (
+    <div className="Abouts">
+     <div className="as-leftSide"><img src="./images/contactus1.jpg" className="as-imageOfAbout"/></div>
+     <div className="as-rightSide"><h1 className="as-title">ABOUT US</h1>
+     <p className="as-text">At DriveEpic, our mission is to revolutionize the car-buying experience by providing a seamless online platform that connects passionate drivers with their dream vehicles, all while ensuring transparency and customer satisfaction.At DriveEpic, our mission is to revolutionize the car-buying experience by providing a seamless online platform that connects passionate drivers with their dream vehicles, all while ensuring transparency and customer satisfaction.</p>
+     <button className="as-button" to="/about">Read More</button></div>
+    </div>
+  );
+}
+
+export default About;
