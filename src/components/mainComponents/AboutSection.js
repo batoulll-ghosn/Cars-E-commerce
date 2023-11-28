d3677a2b5dce6a11a412145b2e539f7cef4700e2
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-11-28 16:02:10 +0200

    about done with responsive

--- src/components/mainComponents/AboutSection.js
+++ src/components/mainComponents/AboutSection.js
@@ -4,7 +4,7 @@ function About() {
     <div className="Abouts">
      <div className="as-leftSide"><img src="./images/contactus1.jpg" className="as-imageOfAbout"/></div>
      <div className="as-rightSide"><h1 className="as-title">ABOUT US</h1>
-     <p className="as-text">At DriveEpic, our mission is to revolutionize the car-buying experience by providing a seamless online platform that connects passionate drivers with their dream vehicles, all while ensuring transparency and customer satisfaction.At DriveEpic, our mission is to revolutionize the car-buying experience by providing a seamless online platform that connects passionate drivers with their dream vehicles, all while ensuring transparency and customer satisfaction.</p>
+     <p className="as-text">At DriveEpic, our mission is to revolutionize the car-buying experience to provide a seamless online platform that connects passionate drivers with their dream vehicles, all while ensuring transparency and customer satisfaction.At DriveEpic, our mission is to revolutionize the car-buying experience to provide a seamless online platform that connects passionate drivers with their dream vehicles, all while ensuring transparency and customer satisfaction.</p>
      <button className="as-button" to="/about">Read More</button></div>
     </div>
   );
