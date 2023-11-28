151de968f43f96667a4d04e6360b6b93334388c5
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-28 23:04:34 +0200

    almost done with header

---
+++ src/components/mainComponents/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/Header.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div className='Header'>  
+        <button className='h-exploremore-btn'>Explore More</button>
+        <button className='h-register-btn'>Register</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
