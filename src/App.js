94fefeb1b18dae25b0335a5384039712746ed0f5
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-28 23:10:24 +0200

    done with Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './components/mainComponents/AboutSection';
 import AboutPage from './components/mainComponents/AboutPage';
+import NavBar from './components/mainComponents/NavBar';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<About />} />
+          <Route path="/" element={<NavBar />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
