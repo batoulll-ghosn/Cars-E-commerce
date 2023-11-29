8cc78c6a4ffc9374f36f45e69a9aa347e55651af
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-29 09:51:03 +0200

    fixed style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './components/styles/style.css';
 import About from './components/mainComponents/AboutSection';
 import AboutPage from './components/mainComponents/AboutPage';
 import NavBar from './components/mainComponents/NavBar';
@@ -8,7 +9,8 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<NavBar />} />
+          {/* <Route path="/navbar" element={<NavBar/>} /> */}
+          <Route path="/" element={ <About /> } />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
