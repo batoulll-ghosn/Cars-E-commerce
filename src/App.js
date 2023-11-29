c84806125132dbd5ddd0751c8665417959159165
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-11-29 15:16:06 +0200

    done with header

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import About from './components/mainComponents/AboutSection';
 import AboutPage from './components/mainComponents/AboutPage';
 import NavBar from './components/mainComponents/NavBar';
 import FAQ from './components/mainComponents/FAQsection';
+import Header from './components/mainComponents/Header';
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
           {/* <Route path="/navbar" element={<NavBar/>} /> */}
-          <Route path="/" element={ <> <NavBar/><About /><FAQ/> </>} />
+          <Route path="/" element={ <> <NavBar/><Header/><About /><FAQ/> </>} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
--- src/components/mainComponents/Header.js
+++ src/components/mainComponents/Header.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import '../styles/Header.css'
-import NavBar from './NavBar';
 
 const Header = () => {
   return (
     <div>
-      <NavBar/>
       <div className='Header'>  
+      <div className='h-main-title'>
+      <p className='h-title'>
+        PURCHASE YOUR   
+      </p>
+      <p className='h-perfect'>PERFECT</p>
+      <p className='h-title'>CAR</p>
+      </div>
+      <div className='h-description'>
+      Over 1000+ New Cars  Available Here
+      </div>
+      <div className='h-buttons'>
         <button className='h-exploremore-btn'>Explore More</button>
         <button className='h-register-btn'>Register</button>
+        </div>
         </div>
     </div>
   )
