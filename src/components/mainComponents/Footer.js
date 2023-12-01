3a867f5592f8da28f4a7f267fcd18b9858a6340b
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-12-01 12:10:13 +0200

    edits to merge

--- src/components/mainComponents/Footer.js
+++ src/components/mainComponents/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ShoppingBag, Share2, Send, Youtube, Facebook, Instagram } from 'react-feather';
 import '../styles/footer.scss';
-
+import '../styles/test.css';
 function Footer() {
  return (
    <footer className="footer">
@@ -12,13 +12,11 @@ function Footer() {
        <div className="footer__parralax-premierplan"></div>
        <div className="footer__parralax-voiture"></div>
      </div>
-     <div className="container">
-       <div className="footer__columns">
-         <div className="footer__col">
-           <h3 className="footer__col-title">
+    <div className="container">
+      <div className="footerFlexContainer">
+        <div className="firstFooterColumn"> <h3 className="footer__col-title">
              <span>DriveEpic</span>
-           </h3>
-           <nav className="footer__nav">
+           </h3><nav className="footer__nav">
              <ul className="footer__nav-list">
                <li className="footer__nav-item">
                  Unparalleled after-sales services
@@ -27,52 +25,28 @@ function Footer() {
                  Security and trust associated with us
                </li>
              </ul>
-           </nav>
-         </div>
-         <div className="footer__col">
-           <h3 className="footer__col-title">
+           </nav></div>
+      <div classname="secondFooterColumn"> <h3 className="footer__col-title">
              <Share2 /> <span>Social Media</span>
-           </h3>
-           <nav className="footer__nav">
-             <ul className="footer__nav-list">
-               <li  className="footer__nav-item" >
-                <a href="" className="footer__nav-link">
-                 
-                </a>
-               </li>
-               <li className="footer__nav-item">
-                <a href="" className="footer__nav-link">
-                
-                </a>
-               </li>
-               <li className="footer__nav-item">
-                <a href="" className="footer__nav-link">
-                 
-                </a>
-               </li>
-             </ul>
-           </nav>
-         </div>
-         <div className="footer__col">
-           <h3 className="footer__col-title">
-             
-           </h3>
-           <nav className="footer__nav">
-             <ul className="footer__nav-list">
+           </h3></div>
+      <div classname="thirdFooterColumn"> <ul className="footer__nav-list">
                <li className="footer__nav-item">
                 <div className='footer-buttons'><button className='Footer-login'>Login</button>
                 <button className='Footer-register'>Register</button></div>
                 
                </li>
-             </ul>
-           </nav>
-         </div>
-       </div>
-       <div className="footer__copyrights">
+             </ul></div>
+    </div>
+    </div>
+    <div className="footer__copyrights">
          <p>Realesed by @DriveEpic</p>
        </div>
-     </div>
-   </footer>
+    
+      
+    
+    
+    
+    </footer>
  );
 }
 
