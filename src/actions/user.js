6716a13d992a4865a334fc3be9fe48bce48782d0
Mhmd Sleimane <mohammad.sleimane@isae.edu.lb>
2023-12-01 10:50:38 +0200

    working on actions and reducers (redux)

---
+++ src/actions/user.js
@@ -0,0 +1,140 @@
+import axios from "axios";
+
+export const getAllUsers = () => {
+  return (dispatch) => {
+    axios
+      .get(`http://localhost:5000/users/getAll`)
+      .then((response) => {
+        const users = response.data.users;
+        dispatch({
+          type: "getAll",
+          payload: users,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+};
+
+export const getUserById = (Id) => {
+  return (dispatch) => {
+    axios
+      .get(`http://localhost:5000/users/findOneById/${Id}`)
+      .then((response) => {
+        const user = response.data.user;
+        dispatch({
+          type: "getUserById",
+          payload: user,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+};
+
+export const getUsersByRole = (role) => {
+  return (dispatch) => {
+    axios
+      .post(`http://localhost:5000/users/findByRole`, { role })
+      .then((response) => {
+        const users = response.data.users;
+        dispatch({
+          type: "getUsersByRole",
+          payload: users
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+};
+
+export const login = (email,password) => {
+    return (dispatch) => {
+      axios
+        .post(`http://localhost:5000/users/login`, { email,password })
+        .then((response) => {
+          const token = response.data.token;
+          dispatch({
+            type: "login",
+            payload: token
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    };
+  };
+
+export const register = (fullName, phoneNumber, email, password, role) => {
+  const newUser = {
+    fullName,
+    phoneNumber,
+    email,
+    password,
+    role,
+  };
+  return (dispatch) => {
+    axios
+      .post(`http://localhost:5000/users/register`, newUser)
+      .then((response) => {
+        const user = response.data.user;
+        dispatch({
+          type: "register",
+          payload: user
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+};
+
+export const deleteUser = (Id) => {
+  return (dispatch) => {
+    axios
+      .delete(`http://localhost:5000/users/deleteById/${Id}`)
+      .then((response) => {
+        dispatch({
+          type: "deleteUser",
+          payload: Id,
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+};
+
+export const updateUser = (
+  Id,
+  fullName,
+  phoneNumber,
+  email,
+  password,
+  role
+) => {
+  const newUser = {
+    fullName,
+    phoneNumber,
+    email,
+    password,
+    role,
+  };
+  return (dispatch) => {
+    axios
+      .put(`http://localhost:5000/users/updateUser/${Id}`, newUser)
+      .then((response) => {
+        const user = response.data.user;
+        dispatch({
+          type: "updateUser",
+          payload: { user, Id },
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import NavBar from './components/mainComponents/NavBar';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import NavBar from "./components/mainComponents/NavBar";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import allReducers from "./reducers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(allReducers, applyMiddleware(thunk));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(store.getState())
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
