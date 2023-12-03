0282a7883a49c14110483244346c570f4209d222
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-12-03 23:32:07 +0200

    done with footer-responsive

--- src/components/mainComponents/Header.js
+++ src/components/mainComponents/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import '../styles/Header.css'
-import NavBar from './NavBar';
 
 const Header = () => {
   return (
     <div>
-      <NavBar/>
       <div className='Header'>  
         <button className='h-exploremore-btn'>Explore More</button>
         <button className='h-register-btn'>Register</button>
