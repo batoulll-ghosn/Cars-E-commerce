529b63cad40233ccac9200133490dc2cfe0688a7
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-12-11 15:01:13 +0200

    nothing

--- src/components/mainComponents/CarViewer.js
+++ src/components/mainComponents/CarViewer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import '../styles/carViewer.css'
 
 const CarViewer = () => {
     
   return (
     <div>
+       <a className='N-logo' href="/">DriveEpic</a>
       <h1> Test</h1>
 
     </div>
