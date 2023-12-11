d7115c346866130c5d19d7ada7a7aa3a0d800b15
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-12-11 16:50:55 +0200

    adding smtp

--- src/components/mainComponents/ContactPage.js
+++ src/components/mainComponents/ContactPage.js
@@ -1,20 +1,47 @@
 import NavBar from "./NavBar"
 import '../styles/contact.css';
-import { useState } from "react";
+import { useState ,useRef} from "react";
+import emailjs from '@emailjs/browser';
 export default function ContactPage(){
+    const form = useRef();
     const [unset,setUnset]=useState(false);
-
+    const [error,setError]=useState(null);
+    const sendEmail = (e) => {
+        e.preventDefault();
+        setError(null);
+        const formData = new FormData(form.current);
+    const userName = formData.get('user_name');
+    const userEmail = formData.get('user_email');
+    const message = formData.get('message');
+    if (!userName || !userEmail || !message) {
+        setError('Please fill in all fields');
+        return;
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(userEmail)) {
+        setError('Please enter a valid email address');
+        return;
+    }
+        emailjs.sendForm('service_l9whsg7', 'template_ac975a9', form.current, 'IhNna7eaQ3PnOGCYp')
+          .then((result) => {
+              console.log(result.text);
+              form.current.reset();
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
     return (
         <>
         <NavBar/>
         <div className={`contact-container ${unset&&`unset-container`}`}>
             <div className='contact-container-d'>
                 <h1>CONTACT Us</h1>
-                <form>
-                    <input type="text" placeholder="Name" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}/>
-                    <input type="text" placeholder="Email" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}/>
-                    <textarea placeholder="Message" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}></textarea>
-                    <button>Submit</button>
+                {error&&<span>{error}</span>}
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="user_name" placeholder="Name" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}/>
+                    <input type="text" name="user_email" placeholder="Email" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}/>
+                    <textarea placeholder="Message" name="message" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}></textarea>
+                    <button >Submit</button>
                 </form>
             </div>
         </div>
