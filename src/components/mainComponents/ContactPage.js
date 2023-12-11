f8716597cd8a1ea99bf39226d2aa636a2f843c0d
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-12-11 10:40:14 +0200

    done with responsive with keyboard issue

--- src/components/mainComponents/ContactPage.js
+++ src/components/mainComponents/ContactPage.js
@@ -1,16 +1,19 @@
 import NavBar from "./NavBar"
 import '../styles/contact.css';
+import { useState } from "react";
 export default function ContactPage(){
+    const [unset,setUnset]=useState(false);
+
     return (
         <>
         <NavBar/>
-        <div className="contact-container">
-            <div className="contact-container-d">
+        <div className={`contact-container ${unset&&`unset-container`}`}>
+            <div className='contact-container-d'>
                 <h1>CONTACT Us</h1>
                 <form>
-                    <input type="text" placeholder="Name"/>
-                    <input type="text" placeholder="Email"/>
-                    <textarea placeholder="Message"></textarea>
+                    <input type="text" placeholder="Name" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}/>
+                    <input type="text" placeholder="Email" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}/>
+                    <textarea placeholder="Message" onFocus={()=>setUnset(true)} onBlur={()=>setUnset(false)}></textarea>
                     <button>Submit</button>
                 </form>
             </div>
