d8dd362437f88068604ed6346fa940909b1350cb
Joya Abou Jaoude <joyaaboujaoude627@gmail.com>
2023-12-13 00:08:57 +0200

    3d-car-viewer working - working on details and style

--- src/components/mainComponents/CarViewer.js
+++ src/components/mainComponents/CarViewer.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import '../styles/carViewer.css'
+import { Canvas } from '@react-three/fiber';
+import { useGLTF, Stage, PresentationControls } from '@react-three/drei';
+
+function Model(props) {
+  const { scene } = useGLTF("/audi_rs7_2014.glb");
+  return <primitive object={scene} {...props}/>
+}
 
 const CarViewer = () => {
     
@@ -12,7 +19,39 @@ const CarViewer = () => {
        </button>
        </div>
        <div className='viewer-3d-viewer'>
-        
+        <Canvas dpr={[1,2]} camera={{fov: 50}} style={{"position": "relative"}}>
+          <color attach="background" args={["#D9D9D9"]} />
+          <PresentationControls speed={3.5} global zoom={.5} polar={[-0.1, Math.PI / 4]} >
+            <Stage environment={"night"}>
+              <Model  scale={0.6} />
+            </Stage>
+          </PresentationControls>
+        </Canvas>
+       </div>
+       
+       <div className='viewer-all-infos'>
+        <div className='viewer-details'>
+        <h3 className='viewer-car-name'>Audi</h3>
+        <p className='viewer-description'>
+        Mercedes-Benz: Where luxury meets performance, crafting iconic vehicles that epitomize elegance 
+        and cutting-edge innovation.
+        </p>
+        <button className='viewer-add-to-cart'>Add to cart</button>
+        </div>
+        <div className='viewer-start-stop'>
+          start
+        </div>
+        <div className='viewer-more-details'>
+          <p className='viewer-description'>
+          Type: sedan
+          </p>
+          <p className='viewer-description'>
+          Type: sedan
+          </p>
+          <p className='viewer-description'>
+          Type: sedan
+          </p>
+        </div>
        </div>
 
       
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import allReducers from "./components/reducers";
+import CarViewer from "./components/mainComponents/CarViewer";
 
 const store = createStore(allReducers, applyMiddleware(thunk));
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -15,7 +16,7 @@ console.log(store.getState())
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <CarViewer />
     </Provider>
   </React.StrictMode>
 );
