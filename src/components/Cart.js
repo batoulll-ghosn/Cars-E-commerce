b80b246098bcf5be9567a9c04a672369ccec34dc
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-12-18 15:11:13 +0200

    adding a new section

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -16,8 +16,21 @@ const Cart=()=>{
                 <tr>
                     <td className='white-tr-td'>Product</td>
                     <td className='white-tr-td'>Details</td>
-                    <td className='white-tr-td'>QTY</td>
                     <td className='white-tr-td'> Price</td>
+                    <td className='white-tr-td'> Action</td>
+                </tr>
+                <tr>
+                    <td className='cart-details-order'><img src={camaro} alt='produc'/></td>
+                    <td className='cart-details-order'>
+                        <span className='cart-carName'>Mercedes A Class</span><br/>
+                        <span className='cart-car-details'>#21345678912334</span><br/>
+                        <span className='cart-car-details'>Sliver// 2018 // Mercedes</span>
+                    </td>
+                    
+                    <td className='cart-details-order'>
+                    <span className='cart-car-details'>19 999$</span>
+                    </td>
+                    <td className='cart-details-order'><span className='cart-car-details'>Delete</span></td>
                 </tr>
                 <tr>
                     <td className='cart-details-order'><img src={camaro} alt='produc'/></td>
@@ -27,27 +40,40 @@ const Cart=()=>{
                         <span className='cart-car-details'>Sliver// 2018 // Mercedes</span>
                     </td>
                     <td className='cart-details-order'>
-                      <span className='cart-car-details-qty'><span>- </span>1 <span>+</span></span>
+                    <span className='cart-car-details'>19 999$</span>
                     </td>
-                    <td className='cart-details-order'>
-                    <span className='cart-car-details-qty'>19 999$</span>
-                    </td>
-                </tr>
-                <tr>
-                    <td className='cart-details-order'><img src={camaro} alt='produc'/></td>
-                    <td className='cart-details-order'>
-                        <span className='cart-carName'>Mercedes A Class</span><br/>
-                        <span className='cart-car-details'>#21345678912334</span><br/>
-                        <span className='cart-car-details'>Sliver// 2018 // Mercedes</span>
-                    </td>
-                    <td className='cart-details-order'>
-                      <span className='cart-car-details-qty'><span>- </span>1 <span>+</span></span>
-                    </td>
-                    <td className='cart-details-order'>
-                    <span className='cart-car-details-qty'>19 999$</span>
-                    </td>
+                    <td className='cart-details-order'><span className='cart-car-details'>Delete</span></td>
                 </tr>
             </table>
+            </div>
+            <div className='footer-cart-container'>
+                <div className='footer-cart-container-white'>
+                    <div className='white-part1'>
+                        <span>Choose Pick Up Location</span>
+                    </div>
+                    <div className='white-part2'>
+                        
+                        <table>
+                            <tr>
+                                <td className='td-white'>SubTotal</td>
+                                <td className='td-n'>45 500 $</td>
+                            </tr>
+                            <tr>
+                                <td className='td-white'>discount</td>
+                                <td className='td-n'>30%-15%</td>
+                            </tr>
+                            <tr>
+                                <td className='part2-shipping'>Shipping</td>
+                                <td className='part2-free'>Free</td>
+                            </tr>
+                            <tr>
+                                <td className='td-white'>TOTAL</td>
+                                <td className='td-n'>42 000 $</td>
+                            </tr>
+                        </table>
+                        <div className='checkout-cart'><button >Checkout 42 000 $</button></div>
+                    </div>
+                </div>
             </div>
 
         </div>
